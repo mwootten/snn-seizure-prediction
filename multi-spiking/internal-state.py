c67215d5a32cc1122ce42df89fd56d90942c789c
Matthew Wootten <the.mattenator.knows@gmail.com>
2017-10-14 16:21:24 -0400

    Organize the repository into folders by parts of the project, and rename files to remove weird names.

--- multi-spiking/internal-state.py
+++ multi-spiking/internal-state.py
@@ -7,23 +7,23 @@ simulationTime = float(input("Simulation Time?"))
 neuronInput = float(input("Neuron Input?"))
 encodingInterval = float(input("Encoding Interval?"))
 refractorinessDecay = float(input("Refractoriness Decay?"))
-# setting constants 
+# setting constants
 synapseDelayA = 1
 synapseDelayB = 1 + simulationTime/4 - (simulationTime%4)/4
 synapseDelayC = synapseDelayB + simulationTime/4 - (simulationTime%4)/4
 synapseDelayD = synapseDelayC + simulationTime/4 - (simulationTime%4)/4
 # calculating synapse delays so that they cover the simulation time
-timeDecay = encodingInterval + 1 
-# setting time decay based on encoding interval 
+timeDecay = encodingInterval + 1
+# setting time decay based on encoding interval
 outputA = 0
 outputB = 0
 outputC = 0
 outputD = 0
 outputE = 0
 # placeholders for outputs
-time = 0 
-# counter for time 
-while time <= 16: 
+time = 0
+# counter for time
+while time <= 16:
   internalState = 0
   if (-neuronInput - synapseDelayA + time) > 0:
     internalState = synapseWeightA * ((-neuronInput - synapseDelayA + time) / timeDecay) * 2.71828 ** (1 - ((-neuronInput - synapseDelayA + time) / timeDecay))
@@ -35,19 +35,19 @@ while time <= 16:
     internalState = internalState + synapseWeightD * ((-neuronInput - synapseDelayB + time) / timeDecay) * 2.71828 ** (1 - ((-neuronInput - synapseDelayD + time) / timeDecay))
   # summing alpha function values for received inputs
   # an input is recieved when the sum of the input time and the delay is equal to the time
-  if outputE > 0: 
+  if outputE > 0:
     internalState = internalState - 2 * 2.71828 ** (-1*(time - outputE)/refractorinessDecay)
-  else: 
-    if outputD > 0: 
+  else:
+    if outputD > 0:
       internalState = internalState - 2 * 2.71828 ** (-1*(time - outputD)/refractorinessDecay)
-    else: 
-      if outputC > 0: 
+    else:
+      if outputC > 0:
         internalState = internalState - 2 * 2.71828 ** (-1*(time - outputC)/refractorinessDecay)
-      else: 
-        if outputB > 0: 
+      else:
+        if outputB > 0:
           internalState = internalState - 2 * 2.71828 ** (-1*(time - outputB)/refractorinessDecay)
         else:
-          if outputA > 0: 
+          if outputA > 0:
             internalState = internalState - 2 * 2.71828 ** (-1*(time - outputA)/refractorinessDecay)
   # adding the refractoriness term for the most recent output
   if internalState > neuronThreshold:
@@ -59,7 +59,7 @@ while time <= 16:
       else:
         if outputC == 0:
           outputC = time
-        else: 
+        else:
           if outputD == 0:
             outputD = time
           else:
