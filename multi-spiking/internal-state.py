ed2adae992f1427a226fbfdc20f736cb27b96dc5
Jeralan <31968280+Jeralan@users.noreply.github.com>
2017-10-15 10:18:25 -0400

    Update internal-state.py
    Added lists

--- multi-spiking/internal-state.py
+++ multi-spiking/internal-state.py
@@ -1,77 +1,38 @@
 neuronThreshold = float(input("Neuron Threshold?"))
-synapseWeightA = float(input("Synapse Weight 1?"))
-synapseWeightB = float(input("Synapse Weight 2?"))
-synapseWeightC = float(input("Synapse Weight 3?"))
-synapseWeightD = float(input("Synapse Weight 4?"))
+synapseNumber = int(input("Synapse Number?"))
+synapseWeight = []
+for x in range (1, (synapseNumber + 1)):
+  synapseWeight.append(float(input("Synapse Weight " + str(x) + "?")))
 simulationTime = float(input("Simulation Time?"))
 neuronInput = float(input("Neuron Input?"))
 encodingInterval = float(input("Encoding Interval?"))
 refractorinessDecay = float(input("Refractoriness Decay?"))
 # setting constants
-synapseDelayA = 1
-synapseDelayB = 1 + simulationTime/4 - (simulationTime%4)/4
-synapseDelayC = synapseDelayB + simulationTime/4 - (simulationTime%4)/4
-synapseDelayD = synapseDelayC + simulationTime/4 - (simulationTime%4)/4
+synapseDelay = [1]
+for x in range (0, synapseNumber-1): 
+  synapseDelay.append(synapseDelay[x] + simulationTime/synapseNumber - (simulationTime%synapseNumber)/synapseNumber)
 # calculating synapse delays so that they cover the simulation time
 timeDecay = encodingInterval + 1
 # setting time decay based on encoding interval
-outputA = 0
-outputB = 0
-outputC = 0
-outputD = 0
-outputE = 0
-# placeholders for outputs
+output = []
+# placeholder for outputs
 time = 0
 # counter for time
 while time <= simulationTime:
   internalState = 0
-  if(neuronInput - synapseDelayA + time) > 0:
-    internalState = synapseWeightA * ((-neuronInput - synapseDelayA + time) / timeDecay) * 2.71828 ** (1 - ((-neuronInput - synapseDelayA + time) / timeDecay))
-  if(neuronInput - synapseDelayB + time) > 0:
-    internalState = internalState + synapseWeightB * ((-neuronInput - synapseDelayB + time) / timeDecay) * 2.71828 ** (1 - ((-neuronInput - synapseDelayB + time) / timeDecay))
-  if(neuronInput - synapseDelayC + time) > 0:
-    internalState = internalState + synapseWeightC * ((-neuronInput - synapseDelayC + time) / timeDecay) * 2.71828 ** (1 - ((-neuronInput - synapseDelayC + time) / timeDecay))
-  if(neuronInput - synapseDelayD + time) > 0:
-    internalState = internalState + synapseWeightD * ((-neuronInput - synapseDelayB + time) / timeDecay) * 2.71828 ** (1 - ((-neuronInput - synapseDelayD + time) / timeDecay))
+  for x in range (0, synapseNumber):
+    if(-neuronInput - synapseDelay[x] + time) > 0:
+      internalState = internalState + synapseWeight[x] * ((-neuronInput - synapseDelay[x] + time)/timeDecay) * 2.71828 ** (1 - ((-neuronInput - synapseDelay[x] + time)/timeDecay))
   # summing alpha function values for received inputs
   # an input is recieved when the sum of the input time and the delay is equal to the time
-  if outputE > 0:
-    internalState = internalState - 2 * 2.71828 ** (-1*(time - outputE)/refractorinessDecay)
-  else:
-    if outputD > 0:
-      internalState = internalState - 2 * 2.71828 ** (-1*(time - outputD)/refractorinessDecay)
-    else:
-      if outputC > 0:
-        internalState = internalState - 2 * 2.71828 ** (-1*(time - outputC)/refractorinessDecay)
-      else:
-        if outputB > 0:
-          internalState = internalState - 2 * 2.71828 ** (-1*(time - outputB)/refractorinessDecay)
-        else:
-          if outputA > 0:
-            internalState = internalState - 2 * 2.71828 ** (-1*(time - outputA)/refractorinessDecay)
+  if len(output[:]) > 0:
+         internalState = internalState - 2 * neuronThreshold * 2.71828 ** (-1*(time - output[-1])/refractorinessDecay)
   # adding the refractoriness term for the most recent output
   if internalState > neuronThreshold:
-    if outputA == 0:
-      outputA = time
-    else:
-      if outputB == 0:
-        outputB = time
-      else:
-        if outputC == 0:
-          outputC = time
-        else:
-          if outputD == 0:
-            outputD = time
-          else:
-            if outputE == 0:
-              outputE = time
+    output.append(time)
   # storing output time if the neuron outputs
   if time == simulationTime:
-    print("Output A is " + str(outputA))
-    print("Output B is " + str(outputB))
-    print("Output C is " + str(outputC))
-    print("Output D is " + str(outputD))
-    print("Output E is " + str(outputE))
+    print("Outputs: " + str(output[:]))
   # final printout of outputs
   time = time + 1
   # counter
