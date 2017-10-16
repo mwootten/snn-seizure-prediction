9ec01ee23dc2c4d45963e14d275e4f88f810a975
Jeralan <31968280+Jeralan@users.noreply.github.com>
2017-10-15 21:57:56 -0400

    Update internal-state.py
    rephrase prompts and add multiple inputs

--- multi-spiking/internal-state.py
+++ multi-spiking/internal-state.py
@@ -1,12 +1,15 @@
 import math
 
 neuronThreshold = float(input("Neuron Threshold?"))
-synapseNumber = int(input("Synapse Number?"))
+synapseNumber = int(input("Number of Synapses?"))
 synapseWeight = []
 for x in range (1, (synapseNumber + 1)):
   synapseWeight.append(float(input("Synapse Weight " + str(x) + "?")))
 simulationTime = float(input("Simulation Time?"))
-neuronInput = float(input("Neuron Input?"))
+inputNumber = int(input("Number of Inputs?"))
+neuronInput = []
+for x in range(1, (inputNumber + 1)):
+  neuronInput.append(float(input("Neuron Input " + str(x) + "?")))
 encodingInterval = float(input("Encoding Interval?"))
 refractorinessDecay = float(input("Refractoriness Decay?"))
 # setting constants
@@ -22,20 +25,21 @@ time = 0
 # counter for time
 while time <= simulationTime:
   internalState = 0
-  for x in range (0, synapseNumber):
-    adjustedTime = -neuronInput - synapseDelay[x] + time
-    if adjustedTime > 0:
-      internalState = internalState + synapseWeight[x] * (adjustedTime/timeDecay) * math.exp(1 - (adjustedTime/timeDecay))
-  # summing alpha function values for received inputs
-  # an input is recieved when the sum of the input time and the delay is equal to the time
+  for y in range (0, inputNumber):
+    for x in range (0, synapseNumber):
+      adjustedTime = -neuronInput[y] - synapseDelay[x] + time
+      if adjustedTime > 0:
+        internalState = internalState + synapseWeight[x] * (adjustedTime/timeDecay) * math.exp(1 - (adjustedTime/timeDecay))
+    # summing alpha function values for received inputs
+    # an input is recieved when the sum of the input time and the delay is equal to the time
   if len(output) > 0:
-         internalState = internalState - 2 * neuronThreshold * math.exp(-1*(time - output[-1])/refractorinessDecay)
-  # adding the refractoriness term for the most recent output
+          internalState = internalState - 2 * neuronThreshold * math.exp(-1*(time - output[-1])/refractorinessDecay)
+    # adding the refractoriness term for the most recent output
   if internalState > neuronThreshold:
-    output.append(time)
+      output.append(time)
   # storing output time if the neuron outputs
   if time == simulationTime:
-    print("Outputs: " + str(output))
+   print("Outputs: " + str(output))
   # final printout of outputs
   time = time + 1
   # counter
