3fc31b9df2170a642e661213b37a646c4047edaf
Matthew Wootten <the.mattenator.knows@gmail.com>
2017-10-30 14:44:01 -0400

    Add structure for looping over neurons

--- multi-spiking/using-networkx.py
+++ multi-spiking/using-networkx.py
@@ -5,15 +5,29 @@ from enum import Enum
 class NeuronType(Enum):
     INPUT = 1
     HIDDEN = 2
-    OUTPUT = 3  # currently not set correctly - fix with low priority
+    OUTPUT = 3
+
+    def __str__(self):
+        lookup = {
+            NeuronType.INPUT: "input",
+            NeuronType.HIDDEN: "hidden",
+            NeuronType.OUTPUT: "output"
+        }
+        return lookup[self]
 
 
 class Neuron:
     """docstring for Neuron."""
-    def __init__(self, spikeTimes, internalState, neuronType):
+    def __init__(self, spikeTimes, internalState, type):
         self.spikeTimes = spikeTimes
         self.internalState = internalState
-        self.neuronType = neuronType
+        self.type = type
+
+    def __repr__(self):
+        return str(self.type)
+
+    def update(self):
+        pass
 
 
 layerSizes = [3, 5, 1]
@@ -21,12 +35,26 @@ synapsesPerConnection = 4
 lastOutput = 16
 
 
-def createLayer(layerSize, neuronType):
-    return [Neuron([], 0, neuronType) for x in range(layerSize)]
+def createLayer(layerSize, type):
+    return [Neuron([], 0, type) for x in range(layerSize)]
 
 
 def synapseDelay(synapseNumber):
     return 1 + synapseNumber * int(lastOutput / synapsesPerConnection)
+
+
+def spikeResponse(time, timeDecay):
+    if time > 0:
+        return (time/timeDecay) * math.exp(1 - (time/timeDecay))
+    else:
+        return 0
+
+
+def refractoriness(time, refractorinessDecay):
+    if time > 0:
+        return -2 * NEURON_THRESHOLD * math.exp(-time / refractorinessDecay)
+    else:
+        return 0
 
 
 G = nx.MultiDiGraph()
@@ -49,3 +77,34 @@ for layerSize in layerSizes:
                         weight=1
                     )
     previousLayer = layer
+
+for outputNeuron in previousLayer:
+    outputNeuron.type = NeuronType.OUTPUT
+
+# some relevent methods:
+# G.predecessors(neuron) - get the presynaptic neurons
+# G.successors(neuron) - get the postsynaptic neurons
+# G.edge[presynaptic][postsynaptic] - get a dict like:
+# {
+#     0: {'delay': 1, 'weight': 1},
+#     1: {'delay': 5, 'weight': 1},
+#     2: {'delay': 9, 'weight': 1},
+#     3: {'delay': 13, 'weight': 1}
+# }
+
+all_presynaptic = [
+    n for n in G.nodes()
+    if (n.type == NeuronType.INPUT)
+    or (n.type == NeuronType.HIDDEN)
+]
+all_postsynaptic = [
+    n for n in G.nodes()
+    if (n.type == NeuronType.HIDDEN)
+    or (n.type == NeuronType.OUTPUT)
+]
+
+for postneuron in all_postsynaptic:
+    preneurons = G.predecessors(postneuron)
+    for preneuron in preneurons:
+        for (synapseNum, associatedData) in G.edge[preneuron][postneuron].items():
+            print(" -- ".join(map(repr, [preneuron, postneuron, synapseNum, associatedData])))
